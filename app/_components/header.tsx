30857632bcd8bbf9e6a1dc4dea9ad71b4467fadb
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-04-30 17:30:40 -0300

    feat: add initial search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,14 +3,18 @@ import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
-    return ( 
-        <div className="flex justify-between px-5 pt-6">
-            <Image src="/Logo.svg" alt="FSW-food" height={30} width={100}/>
-            <Button size={"icon"} variant={'outline'} className="border-none bg-tranparent">
-                <MenuIcon />
-            </Button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Image src="/Logo.svg" alt="FSW-food" height={30} width={100} />
+      <Button
+        size={"icon"}
+        variant={"outline"}
+        className="border-none bg-tranparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
 import Header from "./_components/header";
+import Search from "./_components/search";
+
 
 export default function Home() {
   return (
     <div className="">
-      <Header />
+      <>
+        <Header />
+        <div className="px-5 pt-6">
+          <Search />
+        </div>
+      </>
     </div>
   );
 }
