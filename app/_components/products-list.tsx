77e1e4a5c99dbf0f330f9babc96a18ab37fd72fa
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-04-30 21:21:00 -0300

    feat: add product list and product item componets

---
+++ app/_components/products-list.tsx
@@ -0,0 +1,26 @@
+import { Prisma } from "@prisma/client";
+import ProductItem from "./products-item";
+
+interface ProductListProps {
+  products: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
+
+const ProductList = ({ products }: ProductListProps) => {
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
+import ProductsList from "./_components/products-list";
+import PromoBanner from "./_components/promo-banner";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -15,17 +35,31 @@ export default function Home() {
         <CategoryList />
       </div>
 
-      <div className="pt-6">
-        <Image
+      <div className="px-5 pt-6">
+        <PromoBanner
           src="/Banner-01.svg"
           alt="Até 30% de desconto em pizzas!"
-          height={0}
-          width={0}
-          className="w-full h-auto"
-          sizes="100vh"
-          quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductsList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
       </div>
     </>
   );
-}
+};
+
+export default Home;
