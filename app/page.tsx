ff597044e5afdda88bfe79e5695dd2527ad1d212
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-08 16:24:25 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import Header from "./_components/header";
+
 export default function Home() {
   return (
-    <div className="bg-red-500 px-5 py-4">
-      <h1>Hello world!</h1>
-    </div>
+    <Header />
   );
 }
