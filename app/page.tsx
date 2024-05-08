3130e8640e963d9956eb381e5f00d0e2095427aa
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-08 17:09:12 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import { Fullscreen } from "lucide-react";
 
 export default function Home() {
   return (
@@ -11,6 +13,17 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="pt-6">
+        <Image
+          src="/Banner-01.svg"
+          alt="Até "
+          width={0}
+          height={0}
+          className="h-full w-full"
+          sizes="100vh"
+        />
       </div>
     </>
   );
