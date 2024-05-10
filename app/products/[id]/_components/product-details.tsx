a195fd413f4fa0715009569b7d779c53248b9b26
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-10 15:40:48 -0300

    style: add border radius effect to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -49,7 +49,7 @@ const ProductDetails = ({
     });
 
   return (
-    <div className="py-5">
+    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
       <div className="flex items-center gap-[0.375rem] px-5">
         <div className="relative h-6 w-6">
           <Image
