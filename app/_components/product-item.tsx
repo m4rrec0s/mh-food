01dd410d6c8a09156649fc3a981ea060ab4e9062
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-13 15:17:32 -0300

    feat: add categories page for each category type

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,8 +1,9 @@
 import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
-import { ArrowDownIcon} from "lucide-react";
+import { ArrowDownIcon } from "lucide-react";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -14,13 +15,17 @@ interface ProductItemProps {
       };
     };
   }>;
+  className?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+    <Link
+      className={cn("w-[150px] min-w-[150px]", className)}
+      href={`/products/${product.id}`}
+    >
       <div className="w-full space-y-2">
-        <div className="relative h-[150px] w-full">
+        <div className="relative aspect-square w-full">
           <Image
             src={product.imageUrl}
             alt={product.name}
---
+++ app/category/[id]/products/page.tsx
@@ -0,0 +1,55 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/product-item";
+import RestaurantItem from "@/app/_components/restaurant-item";
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+
+interface CategoriesPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
+  const category = await db.category.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      products: {
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          }
+        },
+      }
+    },
+  });
+
+  if (!category) {
+    return  notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-5 text-lg font-semibold">
+          {category.name}
+        </h2>
+        <div className="grid grid-cols-2 gap-6">
+          {category?.products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+              /> ))}  
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoriesPage;
