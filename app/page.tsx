9f0e6aab7d70f7a0b2fb85442dfc3bf79ef5bc22
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-13 15:47:04 -0300

    refactor: move link inside button

--- app/page.tsx
+++ app/page.tsx
@@ -44,15 +44,17 @@ export default async function Home() {
         <div className="space-y-4 pt-6">
           <div className="flex items-center justify-between px-5">
             <h2 className="font-semibold">Pedidos recomendados</h2>
-            <Link href={`/products/recommended`}>
-              <Button
-                variant="ghost"
-                className="h-fit p-0 text-primary hover:bg-transparent"
-              >
+
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+              asChild
+            >
+              <Link href={`/products/recommended`}>
                 Ver todos
                 <ChevronRightIcon size={16} />
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           </div>
           <ProductList products={products} />
         </div>
@@ -63,19 +65,21 @@ export default async function Home() {
             alt="A partir de 17,90 em lanches"
           />
         </div>
-        
+
         <div className="space-y-4 py-6">
           <div className="flex items-center justify-between px-5">
             <h2 className="font-semibold">Restaurantes recomendados</h2>
-            <Link href="/restaurants/recommended">
-              <Button
-                variant="ghost"
-                className="h-fit p-0 text-primary hover:bg-transparent"
-              >
+
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+              asChild
+            >
+              <Link href="/restaurants/recommended">
                 Ver todos
                 <ChevronRightIcon size={16} />
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           </div>
           <RestaurantList />
         </div>
