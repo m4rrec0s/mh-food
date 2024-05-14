5f6a216498408c5df7fa9887884c6e984c210bce
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-05-14 20:15:10 -0300

    feat: Add quantity parameter to addProductsToCart function

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -3,13 +3,13 @@
 import { Product } from "@prisma/client";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends Product {
   quantity: number;
 }
 
 interface ICartcontext {
   products: CartProduct[];
-  addProductsToCart: (product: Product) => void
+  addProductsToCart: (product: Product, quantity: number) => void;
 }
 
 export const CartContext = createContext<ICartcontext>({
@@ -17,16 +17,34 @@ export const CartContext = createContext<ICartcontext>({
   addProductsToCart: () => {},
 });
 
-export const CartProvider = ({children}: {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([]);
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
-    const addProductsToCart = (product: Product) => {
-        setProducts((prev) => [...prev, { ...product, quantity: 0 }]);
-    };
-
-    return (
-        <CartContext.Provider value={{ products, addProductsToCart }}>
-            {children}
-        </CartContext.Provider>
+  const addProductsToCart = (product: Product, quantity: number) => {
+    const isProductAlreadyInCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
     );
+
+    if (isProductAlreadyInCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+          return cartProduct;
+        })
+      );
+
+    }
+    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+  };
+
+  return (
+    <CartContext.Provider value={{ products, addProductsToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
