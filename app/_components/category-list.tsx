ce7e74766eb64337d10fbf4b02218c9e1101ca1d
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-07-04 15:16:28 -0300

    chore: Update category-list component for responsive design

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,11 +5,22 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="grid grid-cols-2 gap-3">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-3 lg:hidden">
+        {categories.map((category) => (
+          <div key={category.id} className="w-full">
+            <CategoryItem category={category} />
+          </div>
+        ))}
+      </div>
+      <div className="lg:flex gap-5 items-center justify-evenly hidden">
+        {categories.map((category) => (
+          <div key={category.id} className="w-full">
+            <CategoryItem category={category} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
