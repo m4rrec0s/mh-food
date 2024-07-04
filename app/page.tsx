883fc8635ae71030a3a2bc1cb1b9553bce6b4043
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-07-04 15:17:54 -0300

    feat: Update promo banners with new images
    [This commit updates the promo banners on the home page with new images. The previous images were replaced with SVG versions to improve the visual quality and scalability. The first banner now displays a discount on pizzas, while the second banner advertises affordable burgers. This change enhances the visual appeal of the home page and helps attract customers to specific categories.]

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
 import RestaurantList from "./_components/restaurant-list";
 import Link from "next/link";
+import OfferBanner from "./_components/offer-banner";
 
 const fetch = async () => {
   const getProducts = db.product.findMany({
@@ -53,66 +54,82 @@ const Home = async () => {
   return (
     <>
       <Header />
-      <div className="px-5 pt-6">
+      <div className="block px-5 pt-6 lg:hidden">
         <Search />
       </div>
-
-      <div className="px-5 pt-6">
-        <CategoryList />
+      <div className="mt-2 hidden lg:block">
+        <OfferBanner />
       </div>
 
-      <div className="pt-6">
-        <Link href={`/categories/${pizzasCategory?.id}/products`}>
-          <PromoBanner
-            src="/Banner-01.svg"
-            alt="Até 30% de desconto em pizzas!"
-          />
-        </Link>
-      </div>
-
-      <div className="space-y-4 pt-6">
-        <div className="flex items-center justify-between px-5">
-          <h2 className="font-semibold">Pedidos Recomendados</h2>
-
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-            asChild
-          >
-            <Link href="/products/recommended">
-              Ver todos
-              <ChevronRightIcon size={16} />
-            </Link>
-          </Button>
+      <div className="pt-6 lg:px-32 flex flex-col justify-center">
+        <div className="px-5 ">
+          <CategoryList />
         </div>
-        <ProductList products={products} />
-      </div>
-
-      <div className="pt-6">
-        <Link href={`/categories/${burguersCategory?.id}/products`}>
-          <PromoBanner
-            src="/Banner-02.svg"
-            alt="A partir de R$17,90 em lanches"
-          />
-        </Link>
-      </div>
-
-      <div className="space-y-4 py-6">
-        <div className="flex items-center justify-between px-5">
-          <h2 className="font-semibold">Restaurantes Recomendados</h2>
-
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-            asChild
-          >
-            <Link href="/restaurants/recommended">
-              Ver todos
-              <ChevronRightIcon size={16} />
-            </Link>
-          </Button>
+        <div className="pt-6 px-5 lg:hidden">
+          <Link href={`/categories/${pizzasCategory?.id}/products`}>
+            <PromoBanner
+              src="/Banner-01.svg"
+              alt="Até 30% de desconto em pizzas!"
+            />
+          </Link>
         </div>
-        <RestaurantList />
+        <div className="space-y-4 pt-6">
+          <div className="flex items-center justify-between px-5">
+            <h2 className="font-semibold">Pedidos Recomendados</h2>
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+              asChild
+            >
+              <Link href="/products/recommended">
+                Ver todos
+                <ChevronRightIcon size={16} />
+              </Link>
+            </Button>
+          </div>
+          <ProductList products={products} />
+        </div>
+
+        <div className="hidden lg:flex justify-center gap-5 px-5 pt-6">
+          <Link href={`/categories/${pizzasCategory?.id}/products`}>
+              <PromoBanner
+                src="/Banner-01.svg"
+                alt="Até 30% de desconto em pizzas!"
+              />
+            </Link>
+
+            <Link href={`/categories/${burguersCategory?.id}/products`}>
+            <PromoBanner
+              src="/Banner-02.svg"
+              alt="A partir de R$17,90 em lanches"
+            />
+          </Link>
+        </div>
+
+        <div className="pt-6 px-5 lg:hidden">
+          <Link href={`/categories/${burguersCategory?.id}/products`}>
+            <PromoBanner
+              src="/Banner-02.svg"
+              alt="A partir de R$17,90 em lanches"
+            />
+          </Link>
+        </div>
+        <div className="space-y-4 py-6">
+          <div className="flex items-center justify-between px-5">
+            <h2 className="font-semibold">Restaurantes Recomendados</h2>
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+              asChild
+            >
+              <Link href="/restaurants/recommended">
+                Ver todos
+                <ChevronRightIcon size={16} />
+              </Link>
+            </Button>
+          </div>
+          <RestaurantList />
+        </div>
       </div>
     </>
   );
