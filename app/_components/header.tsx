8baa881281858ab27e5e54b7f037a36fb22ce5f8
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-07-05 15:53:04 -0300

    chore: Update header component for responsive design

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
   const handleSignInClick = () => signIn();
 
   return (
-    <div className="flex w-fill justify-between px-5 pt-6 lg:px-32">
+    <div className="flex w-fill justify-between px-5 pt-6 lg:pb-3 lg:px-32 lg:border border-gray-200">
       <Link href="/">
         <div className="relative h-[30px] w-[100px]">
           <Image
